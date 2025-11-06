e3c8e9cce7a5b5c9b2918e8a566e971cda874f5e
ppaksu <smreo082625@gmail.com>
2025-11-06 17:07:06 +0900

    🏗️Controller에서 Service 사용

--- src/main/kotlin/kr/ac/kumoh/s20220457/w25w08_thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20220457/w25w08_thymeleaf/controller/SongController.kt
@@ -1,12 +1,16 @@
 package kr.ac.kumoh.s20220457.w25w08_thymeleaf.controller
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
 class SongController {
     @GetMapping("/songs/random")
-    fun getRandom() : String {
+    fun getRandomSong(model : Model) : String {
+        model.addAttribute(
+            model.addAttribute("songTitle", "Soda Pop")
+        )
         return "random"
     }
 }
---
+++ src/main/kotlin/kr/ac/kumoh/s20220457/w25w08_thymeleaf/model/Song.kt
@@ -0,0 +1,7 @@
+package kr.ac.kumoh.s20220457.w25w08_thymeleaf.model
+
+data class Song(
+    var id: Int,
+    var tittle: String,
+    var singer: String,
+)
